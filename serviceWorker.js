ce60bc15dcc8d3fca3d693bcbcb746da8ca0c9f1
techpro-services <81203287+techpro-services@users.noreply.github.com>
2021-03-25 22:35:04 -0600

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,19 +1,7 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('TechPro-Media').then((cache) => cache.addAll([
-      '/index.html',
-      '/Home.html',
-      '/Resources.html',
-      '/About.html',
-      '/Styles/main.css',
-      '/Styles/iframe.css',
-      '/Images/logo_animated.svg',
-      '/Images/logo_new.svg',
-      '/Images/logo_new.png',
-      '/Images/DPE.svg',
-      '/Images/Curbai.png',
-      '/Images/BiiAndTechPro.svg',
-      '/Images/green_background.jpg'
+      '/index.html'
     ])),
   );
 });
